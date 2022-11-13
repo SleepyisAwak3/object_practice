3e4293981fdf4817db14a15ee05f2c19377d8e89
Noah Byers <noahxbyers@gmail.com>
2022-11-13 15:57:49 -0800

    Add Neat Stuff

---
+++ object_Practice.js
@@ -0,0 +1,12 @@
+var newCar = { 
+  wheels: [4,5,6],
+  brokenWindShield:false,
+  powerSteering: true, 
+  hasRadio: true, 
+  waterDamage: false, 
+  seats: 5,
+} 
+
+console.log(newCar)
+console.log(newCar.wheels)
+console.log(newCar.wheels[2])
